5da297f43435c66d0c76b0f6df13eb952443eed0
Aaron Connolly <aaron.connolly@live.com>
2018-03-26 21:23:37 -0400

    Add binary tree work.

--- DataStructures/CrackingCodeInterview/TreesGraphs/4.2-MinimalTree.playground/Contents.swift
+++ DataStructures/CrackingCodeInterview/TreesGraphs/4.2-MinimalTree.playground/Contents.swift
@@ -1,8 +1,20 @@
 //: Playground - noun: a place where people can play
 
 import Foundation
+import DataStructures
 
 /*
  Minimal Tree:
  Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
+ 
+ Issues:
+ - After each insert, the increasing values are going to make the tree, right-leaning.
+ - So you need to balance after each insert.
+ 
  */
+
+var nums = [3, 4, 5, 6, 8, 10, 12, 16]
+let root = BST<Int>.prepareTree(with: &nums)
+BST.height(root: root)
+
+
